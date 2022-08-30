e5bba65df77ec82f0da213d8d58b7790ba54c69e
vsalagrama <venkateshsalagrama@gmail.com>
2022-08-30 13:50:33 +0530

    Added Blazedemo script for testing

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # PlayWright
+
+https://playwright.dev/docs/running-tests
+
+
+How to run - CLI
+
+npx playwright test
+
+npx playwright test landing-page.spec.ts
+
+npx playwright test tests/todo-page/ tests/landing-page/
+
+npx playwright test landing-page.ts --project=chromium
---
+++ tests/blaze.spec.ts
@@ -0,0 +1,47 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+
+  // Go to https://blazedemo.com/
+  await page.goto('https://blazedemo.com/');
+
+  // Select San Diego
+  await page.locator('select[name="fromPort"]').selectOption('San Diego');
+
+  // Select New York
+  await page.locator('select[name="toPort"]').selectOption('New York');
+
+  // Click text=Find Flights
+  await page.locator('text=Find Flights').click();
+  await expect(page).toHaveURL('https://blazedemo.com/reserve.php');
+
+  // Click text=Choose This Flight 9696 Aer Lingus 5:27 AM 8:22 PM $200.98 >> input[type="submit"]
+  await page.locator('text=Choose This Flight 9696 Aer Lingus 5:27 AM 8:22 PM $200.98 >> input[type="submit"]').click();
+  await expect(page).toHaveURL('https://blazedemo.com/purchase.php');
+
+  // Click [placeholder="First Last"]
+  await page.locator('[placeholder="First Last"]').click();
+
+  // Fill [placeholder="First Last"]
+  await page.locator('[placeholder="First Last"]').fill('venkatesh');
+
+  // Click [placeholder="\31 23 Main St\."]
+  await page.locator('[placeholder="\\31 23 Main St\\."]').click();
+
+  // Fill [placeholder="\31 23 Main St\."]
+  await page.locator('[placeholder="\\31 23 Main St\\."]').fill('address');
+
+  // Click [placeholder="Anytown"]
+  await page.locator('[placeholder="Anytown"]').click();
+
+  // Fill [placeholder="Anytown"]
+  await page.locator('[placeholder="Anytown"]').fill('Kanasas');
+
+  // Click text=Purchase Flight
+  await page.locator('text=Purchase Flight').click();
+  await expect(page).toHaveURL('https://blazedemo.com/confirmation.php');
+
+  // Click pre
+  await page.locator('pre').click();
+
+});
