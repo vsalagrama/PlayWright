6d875a05e33d9704d95509f0ee398763e6cbb24c
Venkatesh <venkateshsalagrama@gmail.com>
2024-05-20 21:20:55 +0530

    Bumped playwrights version to 1.44

--- playwright.config.ts
+++ playwright.config.ts
@@ -64,14 +64,14 @@ const config: PlaywrightTestConfig = {
         ...devices['Desktop Firefox'],
       },
     },
-/*
+
     {
       name: 'webkit',
       use: {
         ...devices['Desktop Safari'],
       },
     },
-*/
+
     /* Test against mobile viewports. */
     // {
     //   name: 'Mobile Chrome',
